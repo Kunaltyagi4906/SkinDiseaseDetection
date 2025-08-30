ee6495303999c31d2c3e1d15ef5969b5c311a2d0
Kunal Tyagi <kunaltyagi4906@gmail.com>
2025-08-30 09:42:45 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,4 @@
-
-
 from skindisease import app, db
 
-# Create tables if not already created
 with app.app_context():
     db.create_all()
-
-# Run the Flask app
-if __name__ == '__main__':
-    app.run(debug=True)
-
-
